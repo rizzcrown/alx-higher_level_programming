e453addcd294aab51f8a338b56cd2ea4388b91f3
rizzcrown <jasonoyato@yahoo.com>
2023-03-06 16:53:59 -0800

    Takle the basics

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "python is an interprated,interactive,object-oriented programming\
+        language that combines remarkable power with very clear syntax"
+str = str[37:64] + " " + str[-22:-18] + " " + str[:6]
+print(str)

d700b6c58b2884125af1bbada338697938547c36
Jason Oyato <jasonoyato@yahoo.com>
2023-03-08 03:26:30 +0300

    python programming

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+for i in range(10):
+    for j in range(i, 10):
+        if i != j:
+            if i == 8 and j == 9:
+                print(f"{i}{j}", end = "")
+            else:
+                print(f"{i}{j}", end = ", ")
